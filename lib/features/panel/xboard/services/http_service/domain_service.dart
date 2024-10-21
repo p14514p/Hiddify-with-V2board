82acb894a90221c4d268976cb11375123bc32d7b
small-haozi <165977055+small-haozi@users.noreply.github.com>
2024-10-22 04:26:21 +0800

    Update domain_service.dart

--- lib/features/panel/xboard/services/http_service/domain_service.dart
+++ lib/features/panel/xboard/services/http_service/domain_service.dart
@@ -1,32 +1,29 @@
-// services/domain_service.dart
-import 'dart:convert';
-import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
-
 class DomainService {
   static const String ossDomain =
       'https://bob-1327268024.cos.ap-shanghai.myqcloud.com/websites.json';
 
-// 从返回的 JSON 中挑选一个可以正常访问的域名
-    static Future<String> fetchValidDomain() async {
-      try {
-          final response = await http
-              .get(Uri.parse(ossDomain))
-              .timeout(const Duration(seconds: 10));
-          
-          final List<dynamic> websites = json.decode(response.body) as List<dynamic>;
-          if (websites.isNotEmpty) {
-              final Map<String, dynamic> websiteMap = websites[0] as Map<String, dynamic>;
-              final String domain = websiteMap['url'] as String;
-              return domain; // 直接返回第一个域名
-          } else {
-              throw Exception('No domains found in the response.');
-          }
-      } catch (e) {
-          if (kDebugMode) {
-              print('Error fetching valid domain: $ossDomain:  $e');
-          }
-          rethrow;
+  // 从返回的 JSON 中挑选一个可以正常访问的域名
+  static Future<String> fetchValidDomain() async {
+    try {
+      final response = await http
+          .get(Uri.parse(ossDomain))
+          .timeout(const Duration(seconds: 10));
+
+      // 直接解析 JSON，不检查状态码
+      final List<dynamic> websites =
+          json.decode(response.body) as List<dynamic>;
+      if (websites.isNotEmpty) {
+        final Map<String, dynamic> websiteMap = websites[0] as Map<String, dynamic>;
+        final String domain = websiteMap['url'] as String;
+        return domain; // 直接返回第一个域名
+      } else {
+        return ''; // 如果没有域名，返回空字符串
       }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error fetching valid domain: $ossDomain:  ${e.toString()}');
+      }
+      return ''; // 捕获异常时返回空字符串
+    }
   }
 }
