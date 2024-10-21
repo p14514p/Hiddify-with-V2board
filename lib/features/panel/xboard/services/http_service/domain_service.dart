023f12d14b20ab716683513db8645b8501bbb9b5
small-haozi <165977055+small-haozi@users.noreply.github.com>
2024-10-22 04:31:32 +0800

    Update domain_service.dart

--- lib/features/panel/xboard/services/http_service/domain_service.dart
+++ lib/features/panel/xboard/services/http_service/domain_service.dart
@@ -1,29 +1,16 @@
+// services/domain_service.dart
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+
 class DomainService {
   static const String ossDomain =
       'https://bob-1327268024.cos.ap-shanghai.myqcloud.com/websites.json';
 
-  // 从返回的 JSON 中挑选一个可以正常访问的域名
+  // 直接返回一个具体的域名
   static Future<String> fetchValidDomain() async {
-    try {
-      final response = await http
-          .get(Uri.parse(ossDomain))
-          .timeout(const Duration(seconds: 10));
-
-      // 直接解析 JSON，不检查状态码
-      final List<dynamic> websites =
-          json.decode(response.body) as List<dynamic>;
-      if (websites.isNotEmpty) {
-        final Map<String, dynamic> websiteMap = websites[0] as Map<String, dynamic>;
-        final String domain = websiteMap['url'] as String;
-        return domain; // 直接返回第一个域名
-      } else {
-        return ''; // 如果没有域名，返回空字符串
-      }
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error fetching valid domain: $ossDomain:  ${e.toString()}');
-      }
-      return ''; // 捕获异常时返回空字符串
-    }
+    // 直接返回你想要的具体链接
+    return 'https://nhyapi.vo0.site'; // 替换为你想要的具体链接
   }
 }
