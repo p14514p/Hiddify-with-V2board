08029418e173ac3694a58f3863d11b5b66f44aae
small-haozi <165977055+small-haozi@users.noreply.github.com>
2024-10-22 04:05:09 +0800

    Update domain_service.dart

--- lib/features/panel/xboard/services/http_service/domain_service.dart
+++ lib/features/panel/xboard/services/http_service/domain_service.dart
@@ -8,47 +8,25 @@ class DomainService {
       'https://bob-1327268024.cos.ap-shanghai.myqcloud.com/websites.json';
 
 // 从返回的 JSON 中挑选一个可以正常访问的域名
-  static Future<String> fetchValidDomain() async {
-    try {
-      final response = await http
-          .get(Uri.parse(ossDomain))
-          .timeout(const Duration(seconds: 10));
-      if (response.statusCode == 200) {
-        final List<dynamic> websites =
-            json.decode(response.body) as List<dynamic>;
-        for (final website in websites) {
-          final Map<String, dynamic> websiteMap =
-              website as Map<String, dynamic>;
-          final String domain = websiteMap['url'] as String;
-          if (await _checkDomainAccessibility(domain)) {
-            if (kDebugMode) {
-              print('Valid domain found: $domain');
-            }
-            return domain;
+    static Future<String> fetchValidDomain() async {
+      try {
+          final response = await http
+              .get(Uri.parse(ossDomain))
+              .timeout(const Duration(seconds: 10));
+          
+          final List<dynamic> websites = json.decode(response.body) as List<dynamic>;
+          if (websites.isNotEmpty) {
+              final Map<String, dynamic> websiteMap = websites[0] as Map<String, dynamic>;
+              final String domain = websiteMap['url'] as String;
+              return domain; // 直接返回第一个域名
+          } else {
+              throw Exception('No domains found in the response.');
           }
-        }
-        throw Exception('No accessible domains found.');
-      } else {
-        throw Exception(
-            'Failed to fetch websites.json: $ossDomain ${response.statusCode}');
+      } catch (e) {
+          if (kDebugMode) {
+              print('Error fetching valid domain: $ossDomain:  $e');
+          }
+          rethrow;
       }
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error fetching valid domain: $ossDomain:  $e');
-      }
-      rethrow;
-    }
-  }
-
-  static Future<bool> _checkDomainAccessibility(String domain) async {
-    try {
-      final response = await http
-          .get(Uri.parse('$domain/api/v1/guest/comm/config'))
-          .timeout(const Duration(seconds: 15));
-
-      return response.statusCode == 200;
-    } catch (e) {
-      return false;
-    }
   }
 }
